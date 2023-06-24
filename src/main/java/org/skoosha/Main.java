93372f87a85583031ecd96ed2eb56de2d2418d40
Sk00sha <filipwagner23@gmail.com>
2023-06-24 15:05:15 +0200

    Adding starter setup for flink dev

---
+++ src/main/java/org/skoosha/Main.java
@@ -0,0 +1,10 @@
+package org.skoosha;
+
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+public class Main {
+    public static void main(String[] args) {
+
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+    }
+}
